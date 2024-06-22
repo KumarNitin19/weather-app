cd11634f9a04c86e4a145e63bf38a2fa667a51b6
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-22 23:48:46 +0530

    fix: typography component fixes

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -13,8 +13,37 @@ type Props = {
   color?: string;
   fontSize?: string;
   children: React.ReactNode | string;
+  fontFamily: "oswald" | "montserrat";
+  className?: string;
 };
 
 export const Typography: React.FC<Props> = (props) => {
-  return <div>{props.children}</div>;
+  //   const className = props.fontFamily || "montserrat";
+  const getClassName = () => {
+    let className = "";
+    if (props.fontFamily) {
+      className += `${props?.fontFamily} `;
+    } else {
+      className += "montserrat ";
+    }
+    if (props.variant) {
+      className += `${props?.variant} `;
+    } else {
+      className += "body1 ";
+    }
+    return className;
+  };
+  return (
+    <div
+      className={`${
+        props.className ? `${props.className} ` : ""
+      }${getClassName()}`}
+      {...props}
+      style={{
+        color: props?.color || "#000",
+        fontSize: props?.fontSize || "16px",
+      }}>
+      {props.children}
+    </div>
+  );
 };
