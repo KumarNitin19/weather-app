303ca0a4d893c74cf8cc3975762d094c7836c5af
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-24 23:54:16 +0530

    update: svg added for bar graph

--- src/components/bar-graph/index.jsx
+++ src/components/bar-graph/index.jsx
@@ -10,5 +10,10 @@ const width = d3.select("#bar_graph").style("width");
 const height = 250;
 
 export const BarGraph = () => {
+  const svg = d3
+    .select("#bar_graph")
+    .append("svg")
+    .attr("height", height - margin.top - margin.bottom)
+    .attr("width", width - margin.left - margin.right);
   return <div id="bar_graph"></div>;
 };
