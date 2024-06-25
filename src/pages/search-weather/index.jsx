e432473f05fe6b68daba3671fd08cdd9a03dd56c
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-26 01:03:35 +0530

    fix: header fixes

--- src/pages/search-weather/index.jsx
+++ src/pages/search-weather/index.jsx
@@ -48,6 +48,17 @@ export default function SearchWeather() {
         <Typography fontFamily="oswald" variant="h6" fontWeight="500">
           WEATHERNOW
         </Typography>
+        <div className="city__location">
+          <svg viewBox="0 0 24 24" tabindex="-1" title="Location">
+            <path d="M12 2c-4.2 0-8 3.22-8 8.2 0 3.32 2.67 7.25 8 11.8 5.33-4.55 8-8.48 8-11.8C20 5.22 16.2 2 12 2m0 10c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2"></path>
+          </svg>
+          <div className="city__location-name">
+            <Typography variant="body2" fontWeight="500">
+              Seattle,
+            </Typography>
+            <Typography variant="body2">Australia</Typography>
+          </div>
+        </div>
       </div>
       <div className="search-weather__city">
         <Input
