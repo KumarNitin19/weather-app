ab1cccb7a18823c0cb0411d78e18e06cd9dae17e
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-06-27 23:54:41 +0530

    feat: weather card implementation

--- src/components/weather-report-card/index.jsx
+++ src/components/weather-report-card/index.jsx
@@ -1,8 +1,48 @@
+import { Typography } from "../typography";
 import "./index.css";
+import Cloud from "../../assets/icons/clouds.png";
+import Rainy from "../../assets/icons/rainy.png";
+import Haze from "../../assets/icons/haze.png";
+import Sunny from "../../assets/icons/sun.png";
+import { Typography } from "../typography";
+
+const icons = {
+  Clouds: Cloud,
+  Haze: Haze,
+  rainy: Rainy,
+  sunny: Sunny,
+  Smoke: Cloud,
+};
 export const WeatherReportCard = () => {
   return (
     <div id="weather-report-card">
-      <div></div>
+      <div>
+        <Typography variant="body2" fontWeight="500">
+          Monday
+        </Typography>
+        <Typography variant="body2">11:42 PM</Typography>
+      </div>
+      <div>
+        <div>
+          <div>
+            <Typography variant="h4" fontWeight="500">
+              16 C
+            </Typography>
+            <div>
+              <Typography variant="caption">Humidity</Typography>
+              <Typography variant="caption" fontWeight="500">
+                51%
+              </Typography>
+            </div>
+          </div>
+          <img
+            src={icons[weatherData?.weather[0]?.main]}
+            alt="Weather icon"
+            height={75}
+            width={75}
+          />
+        </div>
+      </div>
     </div>
   );
 };
