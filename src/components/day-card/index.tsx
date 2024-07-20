c68ff0c9b264da9c7b57631643e8f36d205704c9
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-20 23:16:15 +0530

    feat: weekly and day card added

---
+++ src/components/day-card/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function DayCard() {
+  return (
+    <div className="day__card">
+      <div>Day Card</div>
+    </div>
+  );
+}
---
+++ src/components/weekly-cards/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function WeeklyCard() {
+  return (
+    <div className="weekly__card">
+      <div>Weekly Card</div>
+    </div>
+  );
+}
