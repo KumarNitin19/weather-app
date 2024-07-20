ac76fc374fbb5c9d81abcce5e4727c0c5b42d8c0
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-20 23:36:05 +0530

    feat: mock data added for week cards

--- src/components/day-card/index.tsx
+++ src/components/day-card/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-export default function DayCard() {
+
+type Props = {
+  active: boolean;
+};
+
+export default function DayCard(props: Props) {
+  const { active = false } = props;
+  if (active) {
+    return (
+      <div className="day__card-active">
+        <div>Day Card</div>
+      </div>
+    );
+  }
   return (
     <div className="day__card">
       <div>Day Card</div>
