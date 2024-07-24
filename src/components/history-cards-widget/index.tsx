f2aee7306d37d7645ad96a9feaa105f7c35c5299
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-24 23:01:17 +0530

    feat: data added to history card widget

--- src/components/history-cards-widget/index.tsx
+++ src/components/history-cards-widget/index.tsx
@@ -1,11 +1,17 @@
+import { SEARCH_HISTORY } from "../../mockData/historyData";
 import { CardWrapper } from "../card-wrapper";
 import HistoryCard from "../history-cards";
+import "./style.css";
 
 function HistoryCardWidget() {
   return (
-    <CardWrapper>
-      <HistoryCard />
-    </CardWrapper>
+    <div className="history_card_widget">
+      {SEARCH_HISTORY.map((detail) => (
+        <CardWrapper key={detail.id}>
+          <HistoryCard cardData={detail} />
+        </CardWrapper>
+      ))}
+    </div>
   );
 }
 
