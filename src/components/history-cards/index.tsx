9e59dbf0afd5ef91e17229df3f7dca92b7d21b60
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-24 23:01:37 +0530

    feat: data mapped in history card component

--- src/components/history-cards/index.tsx
+++ src/components/history-cards/index.tsx
@@ -2,25 +2,34 @@ import { WEATHER_ICONS } from "../../contants/commonConstant";
 import { Typography } from "../typography";
 import "./style.css";
 
-function HistoryCard() {
+type IHistoryCardProps = {
+  cardData: {
+    country: string;
+    city: string;
+    weather: string;
+    temprature: number;
+  };
+};
+
+function HistoryCard({ cardData }: IHistoryCardProps) {
   return (
     <div className="history_card">
       <div className="history_card-location">
-        <Typography variant="body2">US</Typography>
+        <Typography variant="body2">{cardData.country}</Typography>
         <Typography variant="h6" fontWeight="500">
-          California
+          {cardData.city}
         </Typography>
-        <Typography variant="body2">Sunny</Typography>
+        <Typography variant="body2">{cardData.weather}</Typography>
       </div>
       <div className="history_card-weather">
         <img
-          src={WEATHER_ICONS["sunny"]}
+          src={WEATHER_ICONS[cardData.weather]}
           height={50}
           width={50}
           alt="Weather icon"
         />
         <Typography variant="h4" fontWeight="500">
-          17° C
+          {cardData.temprature}° C
         </Typography>
       </div>
     </div>
