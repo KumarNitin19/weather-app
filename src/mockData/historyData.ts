521b0626267a5d887eb5ee519326d331c0b136fe
KumarNitin19 <nitinkumardeshmukh19@gmail.com>
2024-07-24 22:55:46 +0530

    feat:mock data added for history cards

---
+++ src/mockData/historyData.ts
@@ -0,0 +1,24 @@
+export const SEARCH_HISTORY = [
+  {
+    id: 0,
+    country: "US",
+    city: "California",
+    weather: "sunny",
+    temprature: 17,
+  },
+  {
+    id: 1,
+    country: "China",
+    city: "Beijing",
+    weather: "rainy",
+    temprature: 19,
+  },
+
+  {
+    id: 2,
+    country: "Israel",
+    city: "Jerusalem",
+    weather: "sunny",
+    temprature: 31,
+  },
+];
